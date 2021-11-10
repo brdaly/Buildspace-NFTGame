9ae6c0ad23cf7befe79bac97d4ea1c0e9b1d0650
Brendan Daly <47114461+brdaly@users.noreply.github.com>
2021-11-10 16:36:17 -0500

    Update run.js

--- run.js
+++ run.js
@@ -1,38 +1,35 @@
-
-
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-      [0, 1, 2],                           
-      ["Leonardo", "Donatello", "Michelangelo"],       
+      ["Leonardo", "Donatello", "Raphael"],       // Names
       ["https://drive.google.com/file/d/1BKHEij9CNl-nP-jrptOaKuk2sCHMUVxm/view?usp=sharing", 
       "https://drive.google.com/file/d/1RruCszZmoJ9fFNNEXF8E95bj5R8irC31/view?usp=sharing", 
       "https://drive.google.com/file/d/1-OTq1Ur8cJ-L8JUD9gACIiIt0b0-q07H/view?usp=sharing"],
-      [100, 80, 90],                    
-      [10, 8, 15]                       
+      [100, 80, 90],                    // HP values
+      [10, 8, 15]                       // Attack damage values
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
+  };
   
-    
     let txn;
     txn = await gameContract.mintCharacterNFT(0);
     txn.wait();
-  
+
     txn = await gameContract.mintCharacterNFT(1);
     txn.wait();
-  
+
     txn = await gameContract.mintCharacterNFT(1);
     txn.wait();
-  
+
     txn = await gameContract.mintCharacterNFT(2);
     txn.wait();
-  
+
     console.log("Done deploying and minting!");
-  
-  };
-  
-  const runMain = async () => {
+
+};
+
+const runMain = async () => {
     try {
       await main();
       process.exit(0);
@@ -40,6 +37,6 @@ const main = async () => {
       console.log(error);
       process.exit(1);
     }
-  };
+};
   
-  runMain();
+runMain();
