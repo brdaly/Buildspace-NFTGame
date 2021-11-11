3ac20829c4e3c0deae52491bd5e6cbc20443f735
Brendan Daly <47114461+brdaly@users.noreply.github.com>
2021-11-11 11:36:37 -0500

    Update TRTLS.sol

--- TRTLS.sol
+++ TRTLS.sol
@@ -3,12 +3,11 @@ pragma solidity ^0.8.0;
 
 // NFT contract to inherit from.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./libraries/Base64.sol";
 
 // Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
-import "./libraries/Base64.sol";
 
 import "hardhat/console.sol";
 
@@ -24,46 +23,6 @@ contract MyEpicGame is ERC721 {
     uint attackDamage;
   }
 
-  struct BigBoss {
-    string name;
-    string imageURI;        
-    uint hp;
-    uint maxHp;
-    uint attackDamage;
-  }
-  
-  BigBoss public bigBoss; 
-
-  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-  CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
-
-  string memory strHp = Strings.toString(charAttributes.hp);
-  string memory strMaxHp = Strings.toString(charAttributes.maxHp);
-  string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
-
-  string memory json = Base64.encode(
-    bytes(
-      string(
-        abi.encodePacked(
-          '{"name": "',
-          charAttributes.name,
-          ' -- NFT #: ',
-          Strings.toString(_tokenId),
-          '", "description": ""This NFT allows people to play as one of the Ninja Turtles!", "image":https://drive.google.com/file/d/1xlsauAHyyUdzG1l8UcKjrz7T69iJBq_7/view?usp=sharing"',
-          charAttributes.imageURI,
-          '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
-          strAttackDamage,'} ]}'
-        )
-      )
-    )
-  );
-
-  string memory output = string(
-    abi.encodePacked("data:application/json;base64,", json)
-  );
-  
-  return output;
-}
   // The tokenId is the NFTs unique identifier, it's just a number that goes
   // 0, 1, 2, 3, etc.
   using Counters for Counters.Counter;
@@ -82,30 +41,13 @@ contract MyEpicGame is ERC721 {
     string[] memory characterNames,
     string[] memory characterImageURIs,
     uint[] memory characterHp,
-    uint[] memory characterAttackDmg,
-    string memory bossName, // These new variables would be passed in via run.js or deploy.js.
-    string memory bossImageURI,
-    uint bossHp,
-    uint bossAttackDamage
-
+    uint[] memory characterAttackDmg
     // Below, you can also see I added some special identifier symbols for our NFT.
     // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
     // Heroes and HERO. Remember, an NFT is just a token!
   )
-    ERC721("Ninja Turtles", "TRTLS")
+    ERC721("Ninja Turtles", "TMNT")
   {
-    
-    // Initialize the boss. Save it to our global "bigBoss" state variable.
-   bigBoss = BigBoss({
-    name: bossName,
-    imageURI: bossImageURI,
-    hp: bossHp,
-    maxHp: bossHp,
-    attackDamage: bossAttackDamage
-   });
-
-  console.log("Done initializing boss %s w/ HP %s, img %s", bigBoss.name, bigBoss.hp, bigBoss.imageURI);  
-    
     for(uint i = 0; i < characterNames.length; i += 1) {
       defaultCharacters.push(CharacterAttributes({
         characterIndex: i,
@@ -119,7 +61,7 @@ contract MyEpicGame is ERC721 {
       CharacterAttributes memory c = defaultCharacters[i];
       
       // Hardhat's use of console.log() allows up to 4 parameters in any order of following types: uint, string, bool, address
-      console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+      console.log("Finished initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
     }
 
     // I increment tokenIds here so that my first NFT has an ID of 1.
@@ -147,22 +89,43 @@ contract MyEpicGame is ERC721 {
       attackDamage: defaultCharacters[_characterIndex].attackDamage
     });
 
-    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex); 
-   
-
+    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+    
     // Keep an easy way to see who owns what NFT.
-   nftHolders[msg.sender] = newItemId;
-  }
-
-  function attackBoss() public {
-  // Get the state of the player's NFT.
-    uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
-    CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
-    console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
-    console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
-  }
+    nftHolders[msg.sender] = newItemId;
 
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
   }
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+  CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+  string memory strHp = Strings.toString(charAttributes.hp);
+  string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+  string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+  string memory json = Base64.encode(
+    bytes(
+      string(
+        abi.encodePacked(
+          '{"name": "',
+          charAttributes.name,
+          ' -- NFT #: ',
+          Strings.toString(_tokenId),
+          '", "description": "This NFT allows people to play as one of the Ninja Turtles!", "image":"https://i.imgur.com/tAwj1AT.jpg"',
+          charAttributes.imageURI,
+          '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+          strAttackDamage,'} ]}'
+        )
+      )
+    )
+  );
+
+  string memory output = string(
+    abi.encodePacked("data:application/json;base64,", json)
+  );
+  
+  return output;
+}
+  
 }
