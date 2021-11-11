9db2c2d9a9cb599287cfd18a996bd2f45a0e5301
Brendan Daly <47114461+brdaly@users.noreply.github.com>
2021-11-11 16:02:08 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,20 +8,70 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   /*
+   * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+   */
+  const [currentAccount, setCurrentAccount] = useState(null);
+  /*
    * Start by creating a new action that we will run on component load
    */
   // Actions
-  const checkIfWalletIsConnected = () => {
-    /*
-     * First make sure we have access to window.ethereum
-     */
-    const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log('Make sure you have MetaMask!');
-      return;
-    } else {
-      console.log('We have the ethereum object', ethereum);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };  
+  /*
+   * Implement your connectWallet method here
+   */
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -36,14 +86,24 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">🐢 Teenage Mutant Ninja Turtles 🐢</p>
+          <p className="header gradient-text"> 🐢 Teenage Mutant Ninja Turtles 🐢 </p>
           <p className="sub-text">Team up to defeat Shredder!</p>
-        </div>
-        <div className="connect-wallet-container">
-          <img
-            src="https://64.media.tumblr.com/6ce0efd032c11117ccefcccee48c8f31/d2f6d1cd1704cba9-91/s500x750/a4e7fb2d40a021ca31ebb5c054c8e2d9385dec26.gifv"
-            alt="Monty Python Gif"
-          />
+          <div className="connect-wallet-container">
+            <img
+              src="https://64.media.tumblr.com/6ce0efd032c11117ccefcccee48c8f31/d2f6d1cd1704cba9-91/s500x750/a4e7fb2d40a021ca31ebb5c054c8e2d9385dec26.gifv"
+              alt="Teenage Mutant Ninja Turtles Gif"
+            />
+            {/*
+             * Button that we will use to trigger wallet connect
+             * Don't forget to add the onClick event to call your method!
+             */}
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
+          </div>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
