5cbb3995ba323d0099864ba5f723c884b0770354
Brendan Daly <47114461+brdaly@users.noreply.github.com>
2021-11-11 11:37:17 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,16 +1,16 @@
-const main = async () => {
+
+  const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(                     
       ["Leonardo", "Donatello", "Raphael"],       // Names
-      ["https://drive.google.com/file/d/1BKHEij9CNl-nP-jrptOaKuk2sCHMUVxm/view?usp=sharing", 
-      "https://drive.google.com/file/d/1RruCszZmoJ9fFNNEXF8E95bj5R8irC31/view?usp=sharing", 
-      "https://drive.google.com/file/d/1-OTq1Ur8cJ-L8JUD9gACIiIt0b0-q07H/view?usp=sharing"],
-      [120, 80, 90],                    // HP values
-      [10, 8, 15]                       // Attack damage values
-    );                     
+      ["https://i.imgur.com/5bjRH49.png", 
+      "https://i.imgur.com/KPP9Ui5.jpg", 
+      "https://i.imgur.com/AJIBSP9.jpg"],
+      [120, 80, 90],                    
+      [10, 8, 15]                       
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
-  
     
     let txn;
     txn = await gameContract.mintCharacterNFT(0);
@@ -24,10 +24,6 @@ const main = async () => {
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
     console.log("Minted NFT #3");
-  
-    txn = await gameContract.mintCharacterNFT(1);
-    await txn.wait();
-    console.log("Minted NFT #4");
   
     console.log("Done deploying and minting!");
   
